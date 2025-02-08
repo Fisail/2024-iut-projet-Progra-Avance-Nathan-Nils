e188754bd6485f5a67728ef8a646a25086e9687f
Nathan H <nathan.hequet@etu.univ-nantes.fr>
2025-02-08 22:47:33 +0100

    fix : correction bug

--- gateway/src/main/kotlin/iut/nantes/project/gateway/Repository/UserRepository.kt
+++ gateway/src/main/kotlin/iut/nantes/project/gateway/Repository/UserRepository.kt
@@ -2,8 +2,10 @@ package iut.nantes.project.gateway.Repository
 
 import iut.nantes.project.gateway.Entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.Optional
 
+@Repository
 interface UserRepository : JpaRepository<UserEntity, Long> {
     fun findByLogin(login: String): Optional<UserEntity>
 }
